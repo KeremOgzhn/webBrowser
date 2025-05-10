391bd7ba4b353888c47b9c4e83394ba302a6a128
Kerem Oğuzhan <keremoguzhan1905@gmail.com>
2025-05-10 15:36:24 +0300

    webBrowser
    C# programlama dilinde yazılmış proje

---
+++ webBrowser/hafta5_webBrowser/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace hafta5_webBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.git = new System.Windows.Forms.Button();
+            this.geri = new System.Windows.Forms.Button();
+            this.ileri = new System.Windows.Forms.Button();
+            this.dur = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser2.Location = new System.Drawing.Point(2, 68);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(586, 434);
+            this.webBrowser2.TabIndex = 1;
+            this.webBrowser2.Url = new System.Uri("https://tbmyo.nku.edu.tr/", System.UriKind.Absolute);
+            this.webBrowser2.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser2_Navigated);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ADRES:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(90, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(780, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // git
+            // 
+            this.git.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.git.Location = new System.Drawing.Point(876, 25);
+            this.git.Name = "git";
+            this.git.Size = new System.Drawing.Size(69, 25);
+            this.git.TabIndex = 4;
+            this.git.Text = "GİT";
+            this.git.UseVisualStyleBackColor = true;
+            this.git.Click += new System.EventHandler(this.git_Click);
+            // 
+            // geri
+            // 
+            this.geri.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.geri.Location = new System.Drawing.Point(951, 25);
+            this.geri.Name = "geri";
+            this.geri.Size = new System.Drawing.Size(69, 25);
+            this.geri.TabIndex = 4;
+            this.geri.Text = "<<";
+            this.geri.UseVisualStyleBackColor = true;
+            this.geri.Click += new System.EventHandler(this.geri_Click);
+            // 
+            // ileri
+            // 
+            this.ileri.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ileri.Location = new System.Drawing.Point(1026, 25);
+            this.ileri.Name = "ileri";
+            this.ileri.Size = new System.Drawing.Size(69, 25);
+            this.ileri.TabIndex = 4;
+            this.ileri.Text = ">>";
+            this.ileri.UseVisualStyleBackColor = true;
+            this.ileri.Click += new System.EventHandler(this.ileri_Click);
+            // 
+            // dur
+            // 
+            this.dur.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dur.Location = new System.Drawing.Point(1101, 25);
+            this.dur.Name = "dur";
+            this.dur.Size = new System.Drawing.Size(69, 25);
+            this.dur.TabIndex = 4;
+            this.dur.Text = "DUR";
+            this.dur.UseVisualStyleBackColor = true;
+            this.dur.Click += new System.EventHandler(this.dur_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(597, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(570, 434);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 506);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ileri);
+            this.Controls.Add(this.dur);
+            this.Controls.Add(this.geri);
+            this.Controls.Add(this.git);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button git;
+        private System.Windows.Forms.Button geri;
+        private System.Windows.Forms.Button ileri;
+        private System.Windows.Forms.Button dur;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ webBrowser/hafta5_webBrowser/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta5_webBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void git_Click(object sender, EventArgs e)
+        {
+            webBrowser2.Navigate(textBox1.Text);
+        }
+
+        private void geri_Click(object sender, EventArgs e)
+        {
+            webBrowser2.GoBack();
+        }
+
+        private void ileri_Click(object sender, EventArgs e)
+        {
+            webBrowser2.GoForward();
+        }
+
+        private void dur_Click(object sender, EventArgs e)
+        {
+            webBrowser2.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser2.ScriptErrorsSuppressed = true;
+        }
+
+        private void webBrowser2_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox1.Text=e.Url.ToString();
+            richTextBox1.Text=webBrowser2.DocumentText;
+            
+        }
+        //1.Yol
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar==13) //ENTER İse
+            {
+                webBrowser2.Navigate(textBox1.Text);
+            }
+        }
+        //2.Yol
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                webBrowser2.Navigate(textBox1.Text);
+            }
+        }
+    }
+}
